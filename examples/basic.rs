b6566a3db8c5532d253bc6857f25d29467d89765
Jan-Erik Rediger <janerik@fnordig.de>
2020-08-14 15:03:09 +0200

    Fix examples

--- examples/basic.rs
+++ examples/basic.rs
@@ -6,15 +6,15 @@ fn main() {
         log::info!("Enqueue {}", i);
         dispatcher::launch(move || {
             log::info!("Executing task {}", i);
-        })
+        }).unwrap();
     }
 
     log::info!("Flushing.");
-    dispatcher::flush_init();
+    dispatcher::flush_init().unwrap();
 
     dispatcher::launch(move || {
         log::info!("Another task!");
-    });
+    }).unwrap();
 
     log::info!("Done");
 }
