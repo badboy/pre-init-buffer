b4debae26af02c6f1ef4e51dbdb8c1f2d26d7c7e
Jan-Erik Rediger <janerik@fnordig.de>
2020-08-14 12:48:42 +0200

    Document it

--- src/global.rs
+++ src/global.rs
@@ -3,20 +3,23 @@ use once_cell::sync::Lazy;
 
 use super::Dispatcher;
 
-const GLOBAL_DISPATCHER_LIMIT: usize = 10;
+const GLOBAL_DISPATCHER_LIMIT: usize = 100;
 static GLOBAL_DISPATCHER: Lazy<RwLock<Dispatcher>> = Lazy::new(|| RwLock::new(Dispatcher::new(GLOBAL_DISPATCHER_LIMIT)));
 
+/// Launch a new task on the global dispatch queue.
+///
+/// The new task will be enqueued immediately.
+/// If the queue was already flushed, a background thread will process tasks in the queue (See `flush_init`).
 pub fn launch(task: impl Fn() + Send + 'static) {
     let mut dispatcher = GLOBAL_DISPATCHER.write().unwrap();
     dispatcher.launch(task)
 }
 
+/// Start processing queued tasks in the global dispatch queue.
+///
+/// This function blocks until queued tasks prior to this call are finished.
+/// Once the initial queue is empty the dispatcher will wait for new tasks to be launched.
 pub fn flush_init() {
     let mut dispatcher = GLOBAL_DISPATCHER.write().unwrap();
     dispatcher.flush_init()
-}
-
-pub fn finish() {
-    let mut dispatcher = GLOBAL_DISPATCHER.write().unwrap();
-    dispatcher.finish()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+//! A global dispatcher queue.
+//!
+//! # Example - Global Dispatch queue
+//!
+//! The global dispatch queue is pre-configured with a maximum queue size of 100 tasks.
+//!
+//! ```rust
+//! // Ensure the dispatcher queue is being worked on.
+//! dispatcher::flush_init();
+//!
+//! dispatcher::launch(|| {
+//!     println!("Executing expensive task");
+//!     // Run your expensive task in a separate thread.
+//! });
+//!
+//! dispatcher::launch(|| {
+//!     println!("A second task that's executed sequentially, but off the main thread.");
+//! });
+//! ```
+
 use std::sync::mpsc::{self, SyncSender};
 use std::thread::{self, JoinHandle};
 
@@ -5,21 +25,46 @@ pub use global::*;
 
 mod global;
 
+/// The command a worker should execute.
 enum Command {
+    /// A task is a user-defined function to run.
     Task(Box<dyn Fn() + Send>),
-    Done,
+    /// Signal the worker to finish work and shut down.
+    Shutdown,
 }
 
+/// A dispatcher.
+///
+/// Run expensive processing tasks sequentially off the main thread.
+/// Tasks are processed in a single separate thread in the order they are submitted.
+/// The dispatch queue will enqueue tasks while not flushed, up to the maximum queue size.
+/// Processing will start after flushing once, processing already enqueued tasks first, then
+/// waiting for further tasks to be enqueued.
+///
+/// # Example
+///
+/// ```rust
+/// # use dispatcher::Dispatcher;
+/// let mut dispatcher = Dispatcher::new(5);
+/// dispatcher.flush_init();
+///
+/// dispatcher.launch(|| {
+///     println!("A task of the main thread");
+/// })
+/// ```
 pub struct Dispatcher {
     preinit_sender: Option<SyncSender<()>>,
     sender: SyncSender<Command>,
-    worker: Option<JoinHandle<()>>,
+    worker: JoinHandle<()>,
 }
 
 impl Dispatcher {
-    pub fn new(maxqueue: usize) -> Self {
+    /// Create a new dispatcher with a maximum queue size.
+    ///
+    /// Launched tasks won't run until `flush_init` is called.
+    pub fn new(max_queue_size: usize) -> Self {
         let (preinit_sender, preinit_rx) = mpsc::sync_channel(1);
-        let (sender, rx) = mpsc::sync_channel(maxqueue);
+        let (sender, rx) = mpsc::sync_channel(max_queue_size);
 
         let worker = thread::spawn(move || {
             log::trace!("Worker started in {:?}", thread::current().id());
@@ -39,8 +84,8 @@ impl Dispatcher {
                 use Command::*;
 
                 match rx.recv() {
-                    Ok(Done) => {
-                        log::trace!("({:?}) Received `Done`", thread::current().id());
+                    Ok(Shutdown) => {
+                        log::trace!("({:?}) Received `Shutdown`", thread::current().id());
                         break;
                     }
                     Ok(Task(f)) => {
@@ -63,10 +108,14 @@ impl Dispatcher {
         Dispatcher {
             preinit_sender: Some(preinit_sender),
             sender,
-            worker: Some(worker),
+            worker,
         }
     }
 
+    /// Start processing queued tasks.
+    ///
+    /// This function blocks until queued tasks prior to this call are finished.
+    /// Once the initial queue is empty the dispatcher will wait for new tasks to be launched.
     pub fn flush_init(&mut self) {
         self.preinit_sender.take().map(|tx| tx.send(()));
 
@@ -81,11 +130,21 @@ impl Dispatcher {
         rx.recv().unwrap();
     }
 
-    pub fn finish(&mut self) {
-        let _ = self.sender.try_send(Command::Done);
-        self.worker.take().map(|w| w.join());
+    /// Shutdown the dispatch queue.
+    ///
+    /// This will initiate a shutdown of the worker thread
+    /// and block until all enqueued tasks are finished.
+    pub fn shutdown(self) {
+        let _ = self.sender.try_send(Command::Shutdown);
+        self.worker.join().unwrap();
     }
 
+    /// Launch a new task on the dispatch queue.
+    ///
+    /// The new task will be enqueued immediately.
+    /// If the queue was already flushed, a background thread will process tasks in the queue (See `flush_init`).
+    ///
+    /// This will not block.
     pub fn launch(&mut self, task: impl Fn() + Send + 'static) {
         match self.sender.try_send(Command::Task(Box::new(task))) {
             Ok(()) => (),
