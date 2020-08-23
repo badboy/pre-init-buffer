7b32e3b3cd91c51c002a4d723aebb6c547be063c
Jan-Erik Rediger <janerik@fnordig.de>
2020-08-23 15:25:32 +0200

    Add missing global example back

---
+++ examples/global_basic.rs
@@ -0,0 +1,27 @@
+use std::{thread, time::Duration};
+
+fn main() {
+    env_logger::init();
+
+    log::info!("Enqueuing.");
+    for i in 1..=10 {
+        log::info!("Enqueue {}", i);
+        dispatcher::launch(move || {
+            log::info!("Executing task {}", i);
+            thread::sleep(Duration::from_millis(50));
+        })
+        .unwrap();
+    }
+
+    log::info!("Flushing.");
+    dispatcher::flush_init().unwrap();
+
+    dispatcher::launch(move || {
+        log::info!("Another task!");
+    })
+    .unwrap();
+
+    dispatcher::try_shutdown().unwrap();
+    dispatcher::join().unwrap();
+    log::info!("Done");
+}
